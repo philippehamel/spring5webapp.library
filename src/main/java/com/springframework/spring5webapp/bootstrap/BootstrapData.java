8245ca7fa47d39778502b179607db25b39c785eb
Philippe Hamel <philippehamel85@gmail.com>
2022-10-12 15:25:36 -0400

    first commit

---
+++ src/main/java/com/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,62 @@
+package com.springframework.spring5webapp.bootstrap;
+
+import com.springframework.spring5webapp.model.Author;
+import com.springframework.spring5webapp.model.Book;
+import com.springframework.spring5webapp.model.Publisher;
+import com.springframework.spring5webapp.repositories.AuthorRepository;
+import com.springframework.spring5webapp.repositories.BookRepository;
+import com.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
+        Publisher quebecor = new Publisher("Quebecor", "123, main street, Montreal");
+        publisherRepository.save(quebecor);
+
+        ddd.setPublisher(quebecor);
+        quebecor.getBooks().add(ddd);
+        publisherRepository.save(quebecor);
+
+        noEJB.setPublisher(quebecor);
+        quebecor.getBooks().add(noEJB);
+        publisherRepository.save(quebecor);
+
+        System.out.println("Number of Publishers: " + publisherRepository.count());
+        System.out.println("Quebecor number of books: " + quebecor.getBooks().size());
+
+        }
+}
---
+++ src/main/java/com/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,82 @@
+package com.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author(String firstName, String lastNAme) {
+        this.firstName = firstName;
+        this.lastName = lastNAme;
+    }
+
+    public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastNAme) {
+        this.lastName = lastNAme;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastNAme='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
